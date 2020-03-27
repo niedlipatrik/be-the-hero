eb9ac0cd76706b599b277addf5094eb9fef5e1a3
niedlipatrik <niedlipatrik@gmail.com>
2020-03-27 19:27:02 +0100

    Conceiot de Estado e  imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+// useState recebe um array com duas informações[valor atual, e função de atualização ]
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter]=useState(0)
+  function incrementar(){
+  setCounter(counter + 1);
+   console.log(counter);
+}
   return (
-    <Header>
-      Semana Omnistack 11
-    </Header>
-  );
+    <div>
+    <Header> Contador: {counter} </Header>
+    <button onClick={incrementar}> Incrementar</button>
+    </div>
+    );
 }
 
 export default App;
