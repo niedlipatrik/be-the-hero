ca12b324cb2e61c721dd55db25b7850c1017b898
niedlipatrik <niedlipatrik@gmail.com>
2020-03-28 01:21:36 +0100

    Layout Frontend com React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
-import React, {useState} from 'react';
+import React from 'react';
+//import React, {useState} from 'react'; -utilizado para o exemplo conter
 // useState recebe um array com duas informações[valor atual, e função de atualização ]
 
-import Header from './Header';
+// import Header from './Header'; ->exemplo de counter
+import './global.css'
+
+import Routes from './routes'
 
 function App() {
-  const [counter, setCounter]=useState(0)
-  function incrementar(){
-  setCounter(counter + 1);
-   console.log(counter);
-}
-  return (
-    <div>
-    <Header> Contador: {counter} </Header>
-    <button onClick={incrementar}> Incrementar</button>
-    </div>
-    );
+// ***** exemplo counter 
+//   const [counter, setCounter]=useState(0)
+//   function incrementar(){
+//   setCounter(counter + 1);
+//    console.log(counter);
+// }
+//   return (
+//     <div>
+//     <Header> Contador: {counter} </Header>
+//     <button onClick={incrementar}> Incrementar</button>
+//     </div>
+//     );
+return (
+<Routes />
+);
 }
 
 export default App;
